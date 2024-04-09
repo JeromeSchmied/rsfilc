4d7a5c28f7972f6628762c64c873985536376930
Jeromos Kovacs <iitsnotme214@proton.me>
2024-04-09 10:26:32 +0200

    fix: unneeded import, switched arg order

--- src/args.rs
+++ src/args.rs
@@ -29,12 +29,12 @@ pub enum Commands {
         /// filter the kind to show
         #[arg(short, long)]
         kind: Option<String>,
-        /// number of entries to show
-        #[arg(short, long, default_value_t = u16::MAX)]
-        number: u16,
         /// average
         #[arg(short, long, default_value_t = false)]
         average: bool,
+        /// number of entries to show
+        #[arg(short, long, default_value_t = u16::MAX)]
+        number: u16,
     },
 
     /// messages the user either recieved or sent
