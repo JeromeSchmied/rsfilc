2c4a6e95fa0a75e749f526a8384baa116182edfd
Jeromos Kovacs <iitsnotme214@proton.me>
2024-05-03 20:28:37 +0200

    fix(user): tests for en/decoding passwords; decode_password() method

--- src/user.rs
+++ src/user.rs
@@ -52,6 +52,15 @@ impl User {
             school_id: school_id.to_string(),
         }
     }
+    /// Returns the decoded password of this [`User`].
+    ///
+    /// # Panics
+    ///
+    /// Panics if decode fails.
+    fn decode_password(&self) -> String {
+        let decoded_password = STANDARD.decode(&self.password).unwrap();
+        String::from_utf8(decoded_password).unwrap()
+    }
     /// creates dummy [`User`], that won't be saved
     pub fn dummy() -> Self {
         info!("created dummy user");
@@ -284,12 +293,11 @@ impl User {
         headers.insert("X-AuthorizationPolicy-Version", "v2".parse().unwrap());
         headers.insert("X-AuthorizationPolicy-Nonce", nonce.parse().unwrap());
 
-        let decoded_password = STANDARD.decode(&self.password)?;
-        let decoded_password = String::from_utf8(decoded_password)?;
+        let decoded_password = self.decode_password();
 
         let mut data = HashMap::new();
         data.insert("userName", self.username.as_str());
-        data.insert("password", &decoded_password /* .decode_64() */);
+        data.insert("password", &decoded_password);
         data.insert("institute_code", &self.school_id);
         data.insert("grant_type", "password");
         data.insert("client_id", endpoints::CLIENT_ID);
@@ -619,11 +627,34 @@ mod tests {
     use super::*;
 
     #[test]
+    fn en_then_decode() {
+        let orig_passwd = "it's_magic×2004.12.30";
+        let usr = User::new("simon", orig_passwd, "klik00000000");
+        let encoded_passwd = &usr.password;
+
+        assert_ne!(orig_passwd, encoded_passwd);
+        assert_eq!(orig_passwd, usr.decode_password());
+    }
+
+    #[test]
+    fn ser_user() {
+        let user = User::new("Test Paul", "2000.01.01", "klik0000001");
+
+        let user_toml = "\
+username = \"Test Paul\"
+password = \"MjAwMC4wMS4wMQ==\"
+school_id = \"klik0000001\"
+";
+
+        assert_eq!(Ok(user_toml.to_owned()), toml::to_string(&user));
+    }
+
+    #[test]
     fn deser_user() {
         let user = toml::from_str(
             r#"
             username = "Test Paul"
-            password = "2000.01.01"
+            password = "MjAwMC4wMS4wMQ=="
             school_id = "klik0000001"
             "#,
         );
@@ -631,18 +662,6 @@ mod tests {
             Ok(User::new("Test Paul", "2000.01.01", "klik0000001")),
             user
         );
-    }
-
-    #[test]
-    fn ser_user() {
-        let user = User::new("Test Paul", "2000.01.01", "klik0000001");
-
-        let user_toml = r#"username = "Test Paul"
-password = "2000.01.01"
-school_id = "klik0000001"
-"#;
-
-        assert_eq!(Ok(user_toml.to_owned()), toml::to_string(&user));
     }
 
     #[test]
@@ -655,12 +674,12 @@ school_id = "klik0000001"
 
         let user_toml = r#"[[users]]
 username = "Test Paul"
-password = "2000.01.01"
+password = "MjAwMC4wMS4wMQ=="
 school_id = "klik0000001"
 
 [[users]]
 username = "Test Paulina"
-password = "2000.01.02"
+password = "MjAwMC4wMS4wMg=="
 school_id = "klik0000002"
 "#;
 
@@ -677,12 +696,12 @@ school_id = "klik0000002"
 
         let user_toml = r#"[[users]]
 username = "Test Paul"
-password = "2000.01.01"
+password = "MjAwMC4wMS4wMQ=="
 school_id = "klik0000001"
 
 [[users]]
 username = "Test Paulina"
-password = "2000.01.02"
+password = "MjAwMC4wMS4wMg=="
 school_id = "klik0000002"
 "#;
 
