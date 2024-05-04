e6fc52dc714e0dac6e2ef5e0606e5b06e0469e32
Jeromos Kovacs <iitsnotme214@proton.me>
2024-05-04 18:38:53 +0200

    fix(msgs): naughtymsgs tests

--- src/messages/tests.rs
+++ src/messages/tests.rs
@@ -125,3 +125,32 @@ fn fix_file_name() {
         f.download_to()
     );
 }
+
+#[test]
+fn naughty_msg_deser() {
+    let msg_s = r#"{
+        "Uid": "862931",
+        "Cim": "Tanévkezdés gimnazistáknak",
+        "Datum": "2023-09-01T13:45:03Z",
+        "KeszitoTanarNeve": "Rendszerüzenet",
+        "KeszitesDatuma": "2023-09-01T13:45:03Z",
+        "LattamozasDatuma": null,
+        "OsztalyCsoport": null,
+        "Tartalom": "\r\nKérjük olvassa el Dr. Pintér Sándor belügyminiszter tanévkezdéshez kapcsolódó, tájékoztató levelét; amelyet az alábbi hivatkozáson keresztül érhet el!\r\n\r\nhttps://tudasbazis.ekreta.hu/download/attachments/2424949/BM_KR%C3%89TA_kommunik%C3%A1ci%C3%B3_gimn.pdf?api=v2\r\n",
+        "TartalomFormazott": "\r\n<p>Kérjük olvassa el Dr. Pintér Sándor belügyminiszter tanévkezdéshez kapcsolódó, tájékoztató levelét; amelyet <strong><a href=\"https://tudasbazis.ekreta.hu/download/attachments/2424949/BM_KR%C3%89TA_kommunik%C3%A1ci%C3%B3_gimn.pdf?api=v2\" target=\"_blank\">IDE</a></strong> kattintva érhet el!</p>\r\n",
+        "Tipus": {
+            "Uid": "5482,ElektronikusUzenet",
+            "Nev": "ElektronikusUzenet",
+            "Leiras": "Elektronikus üzenet"
+        }
+    }"#;
+
+    let msg = serde_json::from_str::<NaughtyMsg>(msg_s);
+    assert!(msg.is_ok());
+
+    let msg = msg.unwrap();
+    assert_eq!(msg.title, "Tanévkezdés gimnazistáknak");
+    assert_eq!(msg.date, "2023-09-01T13:45:03Z");
+    assert_eq!(msg.teacher, "Rendszerüzenet");
+    assert_eq!(msg.msg, "\r\n<p>Kérjük olvassa el Dr. Pintér Sándor belügyminiszter tanévkezdéshez kapcsolódó, tájékoztató levelét; amelyet <strong><a href=\"https://tudasbazis.ekreta.hu/download/attachments/2424949/BM_KR%C3%89TA_kommunik%C3%A1ci%C3%B3_gimn.pdf?api=v2\" target=\"_blank\">IDE</a></strong> kattintva érhet el!</p>\r\n");
+}
