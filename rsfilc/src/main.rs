19228b8d9208a7e87cf00288989572022d9b2a06
Jeromos Kovács <iitsnotme214@proton.me>
2025-02-04 21:55:21 +0100

    refactor(rsfilc): this is not a lib anymore

--- rsfilc/src/main.rs
+++ rsfilc/src/main.rs
@@ -1,12 +1,35 @@
 use chrono::{Datelike, Local};
 use clap::{CommandFactory, Parser};
 use log::*;
-use paths::{delete_cache_dir, log_path};
-use rsfilc::{paths::log_file, *};
+use paths::{delete_cache_dir, log_file, log_path};
 use std::{
     fs::{File, OpenOptions},
     io::Write,
 };
+
+// reexports
+pub use args::{Args, Commands};
+pub use ekreta::Absence;
+pub use ekreta::AnnouncedTest;
+pub use ekreta::Evaluation;
+pub use ekreta::Lesson;
+pub use ekreta::OptIrval;
+pub use ekreta::Res;
+pub use user::User;
+
+pub mod absences;
+pub mod announced;
+pub mod args;
+pub mod cache;
+pub mod endpoints;
+pub mod evals;
+pub mod information;
+pub mod messages;
+pub mod paths;
+pub mod schools;
+mod time;
+pub mod timetable;
+pub mod user;
 
 fn main() -> Res<()> {
     // set up fern
@@ -339,4 +362,29 @@ fn set_up_logger() -> Res<()> {
         // Apply globally
         .apply()?;
     Ok(())
+}
+/// Fill under `this` with many `with` [`char`]s, inlaying `hint` if any.
+///
+/// this:   "123456789" <- len: 9
+/// with:   '#'
+/// hint:   "bab" <- len: 3
+///
+/// so:     "123456789" <- len: 9
+/// result: "12 bab 89" <- len: 9
+pub fn fill(this: &str, with: char, hint: Option<&str>) {
+    let longest = this.lines().map(|l| l.chars().count()).max().unwrap_or(0);
+    let inlay_hint = if let Some(il_hint) = hint {
+        [" ", il_hint, " "].concat()
+    } else {
+        "".to_owned()
+    };
+
+    let left = (longest - inlay_hint.chars().count()) / 2;
+    println!(
+        "{}{}{}",
+        with.to_string().repeat(left),
+        inlay_hint,
+        with.to_string()
+            .repeat(longest - left - inlay_hint.chars().count())
+    );
 }
