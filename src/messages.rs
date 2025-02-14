a66e70eabc77556aa348db6edf6916d3580331a5
Jeromos Kovács <iitsnotme214@proton.me>
2025-02-14 11:47:03 +0100

    refactor(render): more beautiful code

--- src/messages.rs
+++ src/messages.rs
@@ -1,13 +1,9 @@
 //! messages from teachers and staff
 
 use crate::{fill, paths::download_dir, time::MyDate, user::Usr};
-use ekreta::{MsgKind, Res};
-use std::{
-    char,
-    fmt::{self, Write},
-    io::Read,
-    process::{Child, Command, Stdio},
-};
+use ekreta::Res;
+use render_html::Rendr;
+use std::{char, fmt::Write};
 
 pub fn handle(notes: bool, user: &Usr, rev: bool, num: usize) -> Res<()> {
     if notes {
@@ -98,150 +94,153 @@ pub fn disp_msg(msg: &ekreta::MsgItem) -> String {
     f
 }
 
-/// supported external programs that can render html
-enum Rendr {
-    W3m,
-    Lynx,
-}
-impl Rendr {
-    /// Returns the child process needed for this [`Rendr`].
-    fn child(&self) -> Res<Child> {
-        match self {
-            Rendr::W3m => {
-                let mut w3m_cmd = Command::new("w3m");
-                Ok(w3m_cmd
-                    .args([
-                        "-I",
-                        "utf-8",
-                        "-T",
-                        "text/html",
-                        "-o",
-                        "display_link=true",
-                        "-o",
-                        "display_link_number=true",
-                        "-dump",
-                    ])
-                    .stdin(Stdio::piped())
-                    .stdout(Stdio::piped())
-                    .spawn()?)
-            }
-            Rendr::Lynx => {
-                let mut lynx_cmd = Command::new("lynx");
-                Ok(lynx_cmd
-                    .args([
-                        "-stdin",
-                        "-dump",
-                        "-assume_charset",
-                        "utf-8",
-                        "-display_charset",
-                        "utf-8",
-                    ])
-                    .stdin(Stdio::piped())
-                    .stdout(Stdio::piped())
-                    .spawn()?)
-            }
-        }
+mod render_html {
+    use ekreta::Res;
+    use std::io::Write;
+    use std::process::{Child, Command, Stdio};
+    use std::{fmt, io::Read};
+
+    #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+    /// supported external programs that can render html
+    pub enum Rendr {
+        W3m,
+        Lynx,
     }
-    /// Returns the other [`Rendr`].
-    fn other(&self) -> Self {
-        match self {
-            Self::W3m => Self::Lynx,
-            Self::Lynx => Self::W3m,
-        }
-    }
-    /// Returns both [`Rendr`]'s name.
-    fn both_name() -> &'static str {
-        "w3m/lynx"
-    }
-
-    /// render html with a [`Rendr`]
-    fn render_with_external(html: &str, pref: Option<Rendr>) -> Option<String> {
-        let pref = if let Some(pr) = pref { pr } else { Rendr::Lynx };
-
-        let proc = if let Ok(pref_proc) = pref.child() {
-            // eprintln!("rendering with {}", pref);
-            pref_proc
-        } else if let Ok(other_proc) = pref.other().child() {
-            // eprintln!("rendering with {}", pref.other());
-            other_proc
-        } else {
-            eprintln!("couldn't spawn lynx nor w3m, falling back to very-very-basic-html-renderer-written-in-22-lines ;)");
-            return None;
-        };
-
-        if let Err(why) = std::io::Write::write_all(&mut proc.stdin.unwrap(), html.as_bytes()) {
-            eprintln!("couldn't write to {} stdin: {why}", Rendr::both_name());
-            return None;
-        };
-
-        let mut ext_dump = String::new();
-        if let Err(why) = proc.stdout.unwrap().read_to_string(&mut ext_dump) {
-            eprintln!("couldn't read {} stdout: {why}", Rendr::both_name());
-            return None;
-        }
-
-        Some(ext_dump.replace("\\\"", ""))
-    }
-
-    /// Very-Very Basic Html Renderer Written In 22 Lines Of Code: ˝render˝ html to console
-    fn vvbhrwi22loc(html: &str) -> String {
-        let html = html.replace('\\', "");
-
-        let mut text = String::new();
-        let mut is_attr = false;
-        let mut attr = String::new();
-
-        for ch in html.chars() {
-            if ch == '<' {
-                is_attr = true;
-            } else if ch == '>' {
-                is_attr = false;
-
-                if attr.contains('/') {
-                    text.push('\n');
-                }
-            }
-
-            if is_attr {
-                attr.push(ch);
-            } else {
-                attr.clear();
-
-                text.push(ch);
-            }
-        }
-
-        text.replace('>', "").replace("\n\n\n", "\n")
-    }
-    fn render_html(html: &str) -> String {
-        let render_pref = None;
-
-        if let Some(ext) = Self::render_with_external(html, render_pref) {
-            ext
-        } else {
-            Self::vvbhrwi22loc(html)
-        }
-    }
-}
-impl fmt::Display for Rendr {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(
-            f,
-            "{}",
+    impl Rendr {
+        const fn name(self) -> &'static str {
             match self {
                 Rendr::W3m => "w3m",
                 Rendr::Lynx => "lynx",
             }
-        )?;
-        Ok(())
+        }
+        fn args(self) -> &'static [&'static str] {
+            match self {
+                Rendr::W3m => &[
+                    "-I",
+                    "utf-8",
+                    "-T",
+                    "text/html",
+                    "-o",
+                    "display_link=true",
+                    "-o",
+                    "display_link_number=true",
+                    "-dump",
+                ],
+                Rendr::Lynx => &[
+                    "-stdin",
+                    "-dump",
+                    "-assume_charset",
+                    "utf-8",
+                    "-display_charset",
+                    "utf-8",
+                ],
+            }
+        }
     }
-}
 
-pub fn disp_msgkind(msgkind: MsgKind) -> &'static str {
-    match msgkind {
-        MsgKind::Recv => "Beérkezett",
-        MsgKind::Sent => "Elküldve",
-        MsgKind::Del => "Törölve",
+    impl Rendr {
+        /// Returns the child process needed for this [`Rendr`].
+        pub fn child(&self) -> Res<Child> {
+            let mut cmd = Command::new(self.name());
+            Ok(cmd
+                .args(self.args())
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .spawn()?)
+        }
+        /// Returns the other [`Rendr`].
+        pub fn other(&self) -> Self {
+            match self {
+                Self::W3m => Self::Lynx,
+                Self::Lynx => Self::W3m,
+            }
+        }
+        /// Returns both [`Rendr`]'s name.
+        pub fn both_name() -> &'static str {
+            "w3m/lynx"
+        }
+
+        /// render html with a [`Rendr`]
+        pub fn render_with_external(html: &str, pref: Option<Rendr>) -> Option<String> {
+            let pref = if let Some(pr) = pref { pr } else { Rendr::Lynx };
+
+            let proc = if let Ok(pref_proc) = pref.child() {
+                log::info!("rendering with {pref}");
+                pref_proc
+            } else if let Ok(other_proc) = pref.other().child() {
+                log::info!("rendering with {}", pref.other());
+                other_proc
+            } else {
+                eprintln!("couldn't spawn lynx nor w3m, falling back to very-very-basic-html-renderer-written-in-22-lines ;)");
+                return None;
+            };
+
+            Write::write_all(&mut proc.stdin?, html.as_bytes())
+                .inspect_err(|e| eprintln!("couldn't write to {} stdin: {e}", Rendr::both_name()))
+                .ok()?;
+
+            let mut ext_dump = String::new();
+            if let Err(why) = proc.stdout.unwrap().read_to_string(&mut ext_dump) {
+                eprintln!("couldn't read {} stdout: {why}", Rendr::both_name());
+                return None;
+            }
+
+            Some(ext_dump.replace("\\\"", ""))
+        }
+
+        /// Very-Very Basic Html Renderer Written In 22 Lines Of Code: ˝render˝ html to console
+        pub fn vvbhrwi22loc(html: &str) -> String {
+            let html = html.replace('\\', "");
+
+            let mut text = String::new();
+            let mut is_attr = false;
+            let mut attr = String::new();
+
+            for ch in html.chars() {
+                if ch == '<' {
+                    is_attr = true;
+                } else if ch == '>' {
+                    is_attr = false;
+
+                    if attr.contains('/') {
+                        text.push('\n');
+                    }
+                }
+
+                if is_attr {
+                    attr.push(ch);
+                } else {
+                    attr.clear();
+
+                    text.push(ch);
+                }
+            }
+
+            text.replace('>', "").replace("\n\n\n", "\n")
+        }
+        pub fn render_html(html: &str) -> String {
+            let render_pref = None;
+
+            if let Some(ext) = Self::render_with_external(html, render_pref) {
+                ext
+            } else {
+                Self::vvbhrwi22loc(html)
+            }
+        }
+    }
+
+    impl fmt::Display for Rendr {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            writeln!(
+                f,
+                "{}",
+                match self {
+                    Rendr::W3m => "w3m",
+                    Rendr::Lynx => "lynx",
+                }
+            )?;
+            Ok(())
+        }
     }
 }
 
