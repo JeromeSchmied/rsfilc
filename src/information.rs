fb9d56bfd82e7f89762c88c0f3acdd11f5562b7c
Jeromos Kovács <iitsnotme214@proton.me>
2025-02-26 21:09:05 +0100

    fix(disp): user-list is tabled

--- src/information.rs
+++ src/information.rs
@@ -1,22 +1,35 @@
 //! basic User info, `Kréta` stores
 
-use ekreta::UserInfo;
-use std::fmt::Write;
+use crate::user::Usr;
+use ekreta::{Res, UserInfo};
 
-pub fn disp(user_info: &UserInfo, id: &str, def_id: &str) -> String {
-    let mut f = String::new();
+pub fn disp<'a>(users: impl Iterator<Item = &'a Usr>, def_id: &str) -> Res<()> {
+    let mut table = ascii_table::AsciiTable::default();
+    let headers = ["NÉV", "OM AZONOSÍTÓ", "OSKOLA", "SZÜLETETT"];
+    for (i, head) in headers.into_iter().enumerate() {
+        table.column(i).set_header(head);
+    }
+    let data = users
+        .into_iter()
+        .map(|u| {
+            let info = u.get_userinfo()?;
+            Ok(_disp(&info, &u.0.username, def_id))
+        })
+        .collect::<Res<Vec<_>>>()?;
+    table.print(data);
+
+    Ok(())
+}
+
+pub fn _disp(user_info: &UserInfo, id: &str, def_id: &str) -> Vec<String> {
     let def_usr = if id == def_id {
         "Alapértelmezett: "
     } else {
         ""
     };
-    _ = writeln!(&mut f, "| {def_usr}{}, ({id})", user_info.nev);
-    _ = writeln!(&mut f, "| Intézmény: {}", user_info.intezmeny_nev);
-    _ = writeln!(&mut f, "|   id: {}", user_info.intezmeny_azonosito);
-    _ = write!(
-        &mut f,
-        "| Születési dátum: {}",
-        user_info.szuletesi_datum.format("%Y.%m.%d")
-    );
-    f
+    let name = format!("{def_usr}{}", user_info.nev);
+    let id = id.to_string();
+    let school = user_info.intezmeny_nev.clone();
+    let birth = user_info.szuletesi_datum.format("%Y.%m.%d").to_string();
+    vec![name, id, school, birth]
 }
